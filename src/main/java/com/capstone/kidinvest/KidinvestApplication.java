ab75ab8692fc48e0f9fa9a13814774cd9aba14c1
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-03-29 11:34:55 -0500

    initial

---
+++ src/main/java/com/capstone/kidinvest/KidinvestApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.kidinvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidinvestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidinvestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstone/kidinvest/KidinvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.kidinvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidinvestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
