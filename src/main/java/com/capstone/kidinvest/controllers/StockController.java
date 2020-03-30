5153ee1cd8a87517548849169aa5cc62d8e74899
Hung Ly <hunghly@gmail.com>
2020-03-30 17:32:53 -0500

    worked on stock transactions

--- src/main/java/com/capstone/kidinvest/controllers/StockController.java
+++ src/main/java/com/capstone/kidinvest/controllers/StockController.java
@@ -1,7 +1,9 @@
 package com.capstone.kidinvest.controllers;
 
 import com.capstone.kidinvest.models.Stock;
+import com.capstone.kidinvest.models.StockTransaction;
 import com.capstone.kidinvest.repositories.StockRepo;
+import com.capstone.kidinvest.repositories.StockTransactionRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,21 +13,38 @@ import java.util.List;
 @Controller
 public class StockController {
 
-    private StockRepo stockRepo;
+    private StockRepo stockDao;
+    private StockTransactionRepo stockTransactionDao;
 
-    public StockController (StockRepo stockRepo) {
-        this.stockRepo = stockRepo;
+    public StockController(StockRepo stockDao, StockTransactionRepo stockTransactionDao) {
+        this.stockDao = stockDao;
+        this.stockTransactionDao = stockTransactionDao;
     }
 
     @GetMapping("/stocks")
     public String viewStockPage(Model view) {
-        List<Stock> stockList = stockRepo.findAll();
+        List<Stock> stockList = stockDao.findAll();
 
-        for (Stock stock : stockList) {
-            System.out.println(stock.getName());
-        }
+//        for (Stock stock : stockList) {
+//            System.out.println(stock.getName());
+//        }
         view.addAttribute("stocks", stockList);
         return "stock/stock";
+    }
+
+    @GetMapping("/stocks/transactions")
+    public String viewStockTransactionPage(Model view) {
+        List<StockTransaction> stockTransactionsList = stockTransactionDao.findAll();
+
+//        for (StockTransaction stockTransaction : stockTransactionsList) {
+//            System.out.println(stockTransaction.getUser().getUsername());
+//            System.out.println(stockTransaction.getStock().getName());
+//            System.out.println(stockTransaction.getSharesBoughtSold());
+//            System.out.println(stockTransaction.getPrice());
+//            System.out.println(stockTransaction.getTime());
+//        }
+        view.addAttribute("stockTransactions", stockTransactionsList);
+        return "stock/transactions";
     }
 
 
