7e69cdb808f0214afdf93619b08866c43d6e722b
Dayna Leal <dayna.r.leal@gmail.com>
2020-03-30 17:08:35 -0500

    working on business controller and template

--- src/main/java/com/capstone/kidinvest/controllers/BusinessController.java
+++ src/main/java/com/capstone/kidinvest/controllers/BusinessController.java
@@ -1,27 +1,33 @@
 package com.capstone.kidinvest.controllers;
 
+import com.capstone.kidinvest.models.BusinessTransactions;
 import com.capstone.kidinvest.repositories.BusinessRepo;
+import com.capstone.kidinvest.repositories.BusinessTransactionsRepo;
 import com.capstone.kidinvest.repositories.InventoryRepo;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 public class BusinessController {
 
     private BusinessRepo businessDao;
     private InventoryRepo inventoryDao;
+    private BusinessTransactionsRepo businessTransactionsDao;
 
-    public BusinessController(BusinessRepo businessDao, InventoryRepo inventoryDao) {
+    public BusinessController(BusinessRepo businessDao, InventoryRepo inventoryDao, BusinessTransactionsRepo businessTransactionsDao) {
         this.businessDao = businessDao;
         this.inventoryDao = inventoryDao;
+        this.businessTransactionsDao = businessTransactionsDao;
     }
 
     @GetMapping("/business")
-    public String index(Model model) {
-        model.addAttribute("inventory", inventoryDao.findAll());
-        return "business/index";
+    public String viewBusinessPage(Model view) {
+        List<BusinessTransactions> businessTransactionsList = businessTransactionsDao.findAll();
+
+        view.addAttribute("businessTransactions", businessTransactionsList);
+        return "business/business";
     }
-
-
 
     //transactions list
     //sample template to get business transactions listed
