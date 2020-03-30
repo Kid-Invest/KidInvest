ee5aa238e76c08ee72089d38d706af2ea6f48be1
Dayna Leal <dayna.r.leal@gmail.com>
2020-03-30 16:36:20 -0500

    working on business controller

--- src/main/java/com/capstone/kidinvest/controllers/BusinessController.java
+++ src/main/java/com/capstone/kidinvest/controllers/BusinessController.java
@@ -1,13 +1,30 @@
 package com.capstone.kidinvest.controllers;
 
 import com.capstone.kidinvest.repositories.BusinessRepo;
+import com.capstone.kidinvest.repositories.InventoryRepo;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class BusinessController {
 
     private BusinessRepo businessDao;
+    private InventoryRepo inventoryDao;
 
-    public BusinessController(BusinessRepo businessDao) {
+    public BusinessController(BusinessRepo businessDao, InventoryRepo inventoryDao) {
         this.businessDao = businessDao;
+        this.inventoryDao = inventoryDao;
     }
 
+    @GetMapping("/business")
+    public String index(Model model) {
+        model.addAttribute("inventory", inventoryDao.findAll());
+        return "business/index";
+    }
+
+
+
+    //transactions list
+    //sample template to get business transactions listed
+    //display addons
+    //update addons
 }
