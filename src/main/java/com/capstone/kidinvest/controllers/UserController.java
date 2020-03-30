ec5e1d7742266a9361d83968750053aabada917e
Dayna Leal <dayna.r.leal@gmail.com>
2020-03-30 16:12:48 -0500

    added business controller

--- src/main/java/com/capstone/kidinvest/controllers/UserController.java
+++ src/main/java/com/capstone/kidinvest/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.capstone.kidinvest.controllers;
 
 import com.capstone.kidinvest.models.User;
 import com.capstone.kidinvest.repositories.UserRepo;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
 
     private UserRepo userDao;
+    //private PasswordEncoder passwordEncoder;
 
-    private UserController (UserRepo userDao) {
+
+    public UserController(UserRepo userDao) {
         this.userDao = userDao;
+        //this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/login")
+    public String viewLoginPage() {
+        return "/login";
     }
 
     @GetMapping("/register")
@@ -25,8 +34,10 @@ public class UserController {
 
     @PostMapping("/register")
     public String doRegistration(@ModelAttribute User user) {
+        //String hash = passwordEncoder.encode(user.getPassword());
+        //user.setPassword(hash);
         userDao.save(user);
-        return "redirect:/";
+        return "redirect:/login";
     }
 
 }
