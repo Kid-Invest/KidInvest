b078b32af19b8be64a85886fdfe5ba462c256d3d
carlos-delgado23 <carlos.delgado.dev@gmail.com>
2020-03-31 11:19:09 -0500

    test

--- src/main/java/com/capstone/kidinvest/controllers/HelloController.java
+++ src/main/java/com/capstone/kidinvest/controllers/HelloController.java
@@ -1,14 +1,26 @@
 package com.capstone.kidinvest.controllers;
 
+import com.capstone.kidinvest.services.RestService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    @GetMapping("/")
+//    private RestService restService;
 
+    @GetMapping("/")
     public String hello(){
+//        System.out.println(restService.getStocksPlainJSON());
         return "index";
     }
+
+    public static void main(String[] args) {
+         RestService restService;
+
+        System.out.println(restService.getStocksPlainJSON());
+
+    }
+
 }
+
+
