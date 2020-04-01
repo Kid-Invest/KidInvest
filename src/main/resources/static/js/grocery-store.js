d411633753dbd4d88fd52522969a6bc8f44caac6
Hung Ly <hunghly@gmail.com>
2020-04-01 16:22:24 -0500

    worked on store functionality. still need to do crud features

---
+++ src/main/resources/static/js/grocery-store.js
@@ -0,0 +1,38 @@
+{
+    $(document).ready(() => {
+        const subtractBtnEl = $(".subtract-btn");
+        const addBtnEl = $(".add-btn");
+        let newCount = 0;
+        let totalCost = 0;
+
+        // Event listener for add and subtract buttons
+        for (let i = 0; i < subtractBtnEl.length; i++) {
+            subtractBtnEl[i].addEventListener("click", function () {
+                let ingredientEl = $(`#${i}`);
+                let totalCostEl = $(`#${i}_total`);
+                let ingredientCost = $(`#${i}_cost`).html();
+                if (ingredientEl.val() !== '0') {
+                    newCount = parseInt(ingredientEl.val()) - 1;
+                    $(ingredientEl).val(newCount);
+                    totalCostEl.html(
+                        (newCount * parseFloat(ingredientCost)).toFixed(2)
+                    );
+                    totalCost -= parseFloat(ingredientCost);
+                    $("#total_purchase_cost").html(totalCost.toFixed(2));
+                }
+            });
+            addBtnEl[i].addEventListener("click", function () {
+                let ingredientEl = $(`#${i}`);
+                let totalCostEl = $(`#${i}_total`);
+                let ingredientCost = $(`#${i}_cost`).html();
+                newCount = parseInt(ingredientEl.val()) + 1;
+                $(ingredientEl).val(newCount);
+                totalCostEl.html(
+                    (newCount * parseFloat(ingredientCost)).toFixed(2)
+                );
+                totalCost += parseFloat(ingredientCost);
+                $("#total_purchase_cost").html(totalCost.toFixed(2));
+            });
+        }
+    });
+}
