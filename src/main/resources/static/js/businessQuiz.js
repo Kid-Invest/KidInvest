8414a108e5d4aaf2b2a84744722f399aaad72fed
Dayna Leal <dayna.r.leal@gmail.com>
2020-04-06 09:23:10 -0500

    quiz working, need to be able to display after complete

--- src/main/resources/static/js/businessQuiz.js
+++ src/main/resources/static/js/businessQuiz.js
@@ -1,19 +1,132 @@
 {
-    $(document).ready(() => {
-        const purchaseBtnEl = $(".purchase-btn");
-        // Event listener for purchase buttons
-        for (let i = 0; i < purchaseBtnEl.length; i++) {
-            purchaseBtnEl[i].addEventListener("click", function() {
-                let hiddenInputEl = $(`#${i}`);
-                hiddenInputEl.val("true");
-            })
-        }
+    // $(document).ready(() => {
+    //     console.log("hello");
+    //     let incorrectEl = $('.incorrect');
+    //     let correctEl = $('.correct');
+    //
+    //     // Event listener for incorrect answers
+    //     incorrectEl.click(function() {
+    //         $(this).css('color', 'red')
+    //     });
+    //     correctEl.click(function() {
+    //         $(this).css('color', 'green')
+    //     });
+    // });
+    /**
+     * Created with JetBrains WebStorm.
+     * User: pwanwu
+     * Date: 18/09/2013
+     * Time: 17:41
+     * To change this template use File | Settings | File Templates.
+     */
 
-        // let disable = function () {
-        //     let hiddenInputEl = $(`#${i}`);
-        //     if(hiddenInputEl.val() === "true"){
-        //         $(this).prop("disabled", true).html("Purchased");
-        //     }
-        // }
+    var questions = [{
+        question: "question 1?",
+        choices: ["a", "b", "c", "d"],
+        correctAnswer: 1
+    }, {
+        question: "question 2?",
+        choices: ["a", "b", "c", "d"],
+        correctAnswer: 2
+    }, {
+        question: "question 3?",
+        choices: ["a", "b", "c", "d"],
+        correctAnswer: 1
+    }, {
+        question: "question 4?",
+        choices: ["a", "b", "c", "d"],
+        correctAnswer: 0
+    }, {
+        question: "question 5?",
+        choices: ["a", "b", "c", "d"],
+        correctAnswer: 0
+    }];
+
+    var currentQuestion = 0;
+    var correctAnswers = 0;
+    var quizOver = false;
+
+    $(document).ready(function () {
+
+        // Display the first question
+        displayCurrentQuestion();
+        $(this).find(".quizMessage").hide();
+
+        // On clicking next, display the next question
+        $(this).find(".nextButton").on("click", function () {
+            if (!quizOver) {
+
+                value = $("input[type='radio']:checked").val();
+
+                if (value == undefined) {
+                    $(document).find(".quizMessage").text("Please select an answer");
+                    $(document).find(".quizMessage").show();
+                } else {
+                    // TODO: Remove any message -> not sure if this is efficient to call this each time....
+                    $(document).find(".quizMessage").hide();
+
+                    if (value == questions[currentQuestion].correctAnswer) {
+                        correctAnswers++;
+                    }
+
+                    currentQuestion++; // Since we have already displayed the first question on DOM ready
+                    if (currentQuestion < questions.length) {
+                        displayCurrentQuestion();
+                    } else {
+                        displayScore();
+                        //                    $(document).find(".nextButton").toggle();
+                        //                    $(document).find(".playAgainButton").toggle();
+                        // Change the text in the next button to ask if user wants to play again
+                        $(document).find(".nextButton").text("Play Again?");
+                        quizOver = true;
+                    }
+                }
+            } else { // quiz is over and clicked the next button (which now displays 'Play Again?'
+                quizOver = false;
+                $(document).find(".nextButton").text("Next Question");
+                resetQuiz();
+                displayCurrentQuestion();
+                hideScore();
+            }
+        });
+
     });
+
+// This displays the current question AND the choices
+    function displayCurrentQuestion() {
+
+        console.log("In display current Question");
+
+        var question = questions[currentQuestion].question;
+        var questionClass = $(document).find(".quizContainer > .question");
+        var choiceList = $(document).find(".quizContainer > .choiceList");
+        var numChoices = questions[currentQuestion].choices.length;
+
+        // Set the questionClass text to the current question
+        $(questionClass).text(question);
+
+        // Remove all current <li> elements (if any)
+        $(choiceList).find("li").remove();
+
+        var choice;
+        for (i = 0; i < numChoices; i++) {
+            choice = questions[currentQuestion].choices[i];
+            $('<li><input type="radio" value=' + i + ' name="dynradio" />' + choice + '</li>').appendTo(choiceList);
+        }
+    }
+
+    function resetQuiz() {
+        currentQuestion = 0;
+        correctAnswers = 0;
+        hideScore();
+    }
+
+    function displayScore() {
+        $(document).find(".quizContainer > .result").text("You scored: " + correctAnswers + " out of: " + questions.length);
+        $(document).find(".quizContainer > .result").show();
+    }
+
+    function hideScore() {
+        $(document).find(".result").hide();
+    }
 }
