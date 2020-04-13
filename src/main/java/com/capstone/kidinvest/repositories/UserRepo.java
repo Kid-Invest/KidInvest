c47c9927be7d8c35a9de54cc2b194c0de3dd3b89
Hung Ly <hunghly@gmail.com>
2020-04-13 18:30:19 -0500

    added registration check for email

--- src/main/java/com/capstone/kidinvest/repositories/UserRepo.java
+++ src/main/java/com/capstone/kidinvest/repositories/UserRepo.java
@@ -8,5 +8,6 @@ import java.util.List;
 public interface UserRepo extends JpaRepository<User, Long> {
     User findUserById(long id);
     User findByUsername(String username);
+    User findByEmail(String email);
     List<User> findAll();
 }
